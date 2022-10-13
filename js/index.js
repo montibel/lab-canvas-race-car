628a48da992c7dea9e4b9bb4598c8f7ace378e6b
“Marcela <mpmontiela@gmail.com>
2022-10-13 14:40:58 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+
+const canvas = document.querySelector("canvas");
+canvas.style.border = "2px solid black";
+const ctx = canvas.getContext("2d");
+
+const background = new Image();
+background.src = "../images/road.png";  
+const car = new Image();
+car.src ="../images/car.png";  
+
 window.onload = () => {
+  
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height
+    );
+  ctx.drawImage(car, 200, 500, 70, 100);
   document.getElementById('start-button').onclick = () => {
+    console.log("starting");
     startGame();
   };
 
   function startGame() {}
 };
+ 
