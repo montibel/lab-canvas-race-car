6dc821cf0426879c2b87bcb29706593c4a1be5c9
“Marcela <mpmontiela@gmail.com>
2022-10-13 18:19:43 +0200

    done

--- js/index.js
+++ js/index.js
@@ -11,10 +11,11 @@ const car = new Image();
 car.src = "../images/car.png"; 
 let bgy = 0;
 let bgy2 = -canvas.height;
-
+let newCar = 200 
 let isGameOver = false;
 let gameId = 0;
-
+let moveRight = false;
+let moveLeft = false;
 
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
@@ -22,14 +23,20 @@ window.onload = () => {
     startGame();
   };
 
+
   function startGame() {
     console.log(gameId);
     startScreen.style.display = "none"; 
     ctx.drawImage(background, 0, bgy, canvas.width, canvas.height);
     ctx.drawImage(background2, 0, bgy2, canvas.width, canvas.height);
-    ctx.drawImage(car, 200, 500, 70, 100);
+    ctx.drawImage(car, newCar, 500, 70, 100);
     bgy += 3;
     bgy2 += 3;
+    
+    
+  if (moveLeft === true) {newCar -= 2};
+  if (moveRight === true) {newCar += 2}; 
+
 
   if (bgy > canvas.height) {
     bgy = -canvas.height -20; 
@@ -46,7 +53,28 @@ window.onload = () => {
 
 if (gameId === 2000) {
    isGameOver = true;
-}  
+   }  
   }
-};
+
+  const moveCarLeft = () => {
+    if (carX >= 0 + 50) {
+      carX -= 5;
+    }
+  }
+  
+  document.addEventListener("keydown", event => {
+    console.log(event); 
+    if (event.code === "ArrowLeft") {
+      moveLeft = true;
+    }
+    if (event.code === "ArrowRight") {
+      moveRight = true; 
+    }
+  })
+  document.addEventListener("keyup", event => {
+   moveLeft = false;
+   moveRight = false; 
+  })
+ }
+ 
  
